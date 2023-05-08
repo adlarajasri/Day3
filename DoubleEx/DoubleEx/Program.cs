a239938a0718dd79e67ea19eef333453099877c8
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-08 10:09:35 +0530

    Add files via upload

---
+++ DoubleEx/DoubleEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleEx
+{ 
+        class raji
+        {
+            static void Main(string[] args)
+            {
+                int n = 10;
+                double raji = 20.50;
+                Console.WriteLine("raji");
+            }
+        }
+    }
+
+    
+
